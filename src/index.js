93179c4d14d30325d4d9a0a4d8a5b635dc0bb931
sugam <sugambhargav@gmail.com>
2023-11-25 22:41:07 +0530

    Improve code readability by placing code in functions

--- src/index.js
+++ src/index.js
@@ -1,39 +1,38 @@
-const todoText = document.querySelector("#todolist")
-const mainContainer = document.querySelector(".container")
+const todoList1 = todoList();
+const todos = todoList1.todos;
+const todoText = document.querySelector("#todo-content");
+const form = document.querySelector('#form');
+const inputContent = document.querySelector("#todo");
 
 function todoList() {
     return { todos: [] };
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const todoList1 = todoList();
-    const todos = todoList1.todos;
 
-    const form = document.querySelector('form');
-    const inputContent = document.querySelector("#todo");
-
+function todoPusher() {
     form.addEventListener('submit', function (e) {
-        e.preventDefault()
-        
-        const newTodo = inputContent.value;
-        form.reset()
-        
+    e.preventDefault();
 
-        if (newTodo !== "") {
-            todos.push(newTodo);
-            updateToDoList()    
-        }
+    const newTodo = inputContent.value;
+    form.reset();
 
-        function updateToDoList() {
-            todoText.textContent = ""
-
-            todos.forEach(todo => {
-            const todoItem = document.createElement('li');
-            todoItem.textContent = todo
-            todoText.append(todoItem)
-    })
+    if (newTodo !== "") {
+        todos.push(newTodo);
+        updateToDoList();
+    }
+});
 }
 
-    });
-});
 
+function updateToDoList() {
+    todoText.textContent = "";
+
+    todos.forEach(todo => {
+        const todoItem = document.createElement('li');
+        todoItem.textContent = todo;
+        todoText.append(todoItem);
+    });
+}
+
+
+todoPusher()
